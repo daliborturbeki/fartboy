2bf8380fb0d55b0e9102682f8c2d37e3e3cf67f1
dturbeki <dturbeki@omega-software.hr>
2025-01-18 12:18:39 +0100

    added steps to buy coin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,6 +9,30 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
+      <Row className="align-items-center">
+          <Col size={12}>
+            <h2 style={{color: 'white'}}>How to Buy Farts</h2>
+            <ol style={{color: 'white'}}>
+              <li>
+                <strong>Access DexScreener:</strong>
+                <p>Visit <a href="https://dexscreener.com" style={{color: 'white'}}>dexscreener.com</a> through your browser or use the DexScreener app for easy access.</p>
+              </li>
+              <li>
+                <strong>Search contract address:</strong>
+                <p>Contact address is <b>y1AZt42vceCmStjW4zetK3VoNarC1VxJ5iDjpiupump</b>. You can access it anytime via our website, X account, or join the Telegram group for quick updates.</p>
+              </li>
+              <li>
+                <strong>Buy Farts:</strong>
+                <p>To make a purchase, select the amount you'd like to buy, adjust your slippage settings, and click the <i>Buy</i> button. When you're ready to sell, follow the same steps but choose the red <i>Sell</i> button before confirming. However, I recommend holding off on selling for now! 👀</p>
+              </li>
+            </ol>
+          </Col>
+        </Row>
+        <Row>
+          <Col size={12}>
+            <hr style={{borderTop: '1px solid white'}} />
+          </Col>
+        </Row>
         <Row className="align-items-center">
           <Col size={12} sm={6}>
             <h1 style={{color: 'white'}}>FartBoy</h1>
