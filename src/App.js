f3407643cf1c9f4dda338ec02a2dd839dcc3234f
dturbeki <dturbeki@omega-software.hr>
2025-01-19 12:55:39 +0100

    editing x link preview

--- src/App.js
+++ src/App.js
@@ -25,21 +25,22 @@ function App() {
 
   return (
     <div className="App">
-    <Helmet>
-      <script type="application/ld+json">
-        {JSON.stringify(jsonLdData)}
-      </script>
-      <meta property="og:title" content="FartBoy" />
-      <meta property="og:description" content="FartBoy is a playful and community-focused cryptocurrency inspired by the hilarious and heroic spirit of the comic book character." />
-      <meta property="og:image" content="https://www.fartboysol.com/static/media/fartboy_2.6e1751091c05c4cff2bf.png" />
-      <meta property="og:url" content="https://www.fartboysol.com" />
-      <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="FartBoy" />
-      <meta name="twitter:description" content="FartBoy is a playful and community-focused cryptocurrency inspired by the hilarious and heroic spirit of the comic book character." />
-      <meta name="twitter:image" content="https://www.fartboysol.com/static/media/fartboy_2.6e1751091c05c4cff2bf.png" />
-      <meta name="twitter:image:alt" content="FartBoy" />
-    </Helmet>
+      <Helmet>
+        <script type="application/ld+json">
+          {JSON.stringify(jsonLdData)}
+        </script>
+        <meta property="og:title" content="FartBoy" />
+        <meta property="og:description" content="FartBoy is a playful and community-focused cryptocurrency inspired by the hilarious and heroic spirit of the comic book character." />
+        <meta property="og:image" content="https://www.fartboysol.com/static/media/fartboy_2.6e1751091c05c4cff2bf.png" />
+        <meta property="og:url" content="https://www.fartboysol.com" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@FARTBOY_CTO" />
+        <meta name="twitter:title" content="FartBoy - The Fun Cryptocurrency" />
+        <meta name="twitter:description" content="FartBoy is a playful and community-focused cryptocurrency inspired by the hilarious and heroic spirit of the comic book character." />
+        <meta name="twitter:image" content="https://www.fartboysol.com/static/media/fartboy_2.6e1751091c05c4cff2bf.png" />
+        <meta name="twitter:image:alt" content="FartBoy" />
+      </Helmet>
       <NavBar />
       <Banner />
       <Tokenomics />
