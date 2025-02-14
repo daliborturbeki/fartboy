dbac9eb1fe0cf0ff0139be7e5fb592cffe66ff73
dturbeki <dturbeki@omega-software.hr>
2025-02-14 12:47:01 +0100

    modifying game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,9 +7,8 @@ export const Game = () => {
   return (
     <section className="game" id="game">
       <Container className="text-center"> 
-        <button onClick={() => window.location.href = '/'} style={{position: 'absolute', top: '20px', left: '10px'}}>
+        <button onClick={() => window.location.href = '/'} style={{position: 'absolute', top: '20px', left: '20px'}} title="Go back">
           <ArrowLeftCircle size={55} color="white" />
-          <span className="fs-4">Go Back</span>
         </button>
         <iframe frameborder="0" src="https://itch.io/embed-upload/12738277?color=4f6781" allowfullscreen="" width="1280" height="740">
           <a href="https://scranstudios.itch.io/fartboy-flies-again">Play FartBoy Flies Again! on itch.io</a>
