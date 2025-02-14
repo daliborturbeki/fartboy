3719f95768aa5e7bb619d3f6b3d91a4eeff13c15
dturbeki <dturbeki@omega-software.hr>
2025-02-14 19:22:50 +0100

    adjusting the game for the mobile

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,17 +2,26 @@ import { Container } from "react-bootstrap"
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ArrowLeftCircle } from "react-bootstrap-icons";
+import { useMediaQuery } from 'react-responsive';
 
 export const Game = () => {
+  const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 869px)' });
+
   return (
     <section className="game" id="game">
-      <Container className="text-center"> 
-        <button onClick={() => window.location.href = '/'} style={{position: 'absolute', top: '20px', left: '20px'}} title="Go back">
-          <ArrowLeftCircle size={55} color="white" />
-        </button>
-        <iframe frameborder="0" src="https://itch.io/embed-upload/12738277?color=4f6781" allowfullscreen="" width="1280" height="740">
-          <a href="https://scranstudios.itch.io/fartboy-flies-again">Play FartBoy Flies Again! on itch.io</a>
-        </iframe>
+      <Container>
+        {isDesktopOrLaptop && (
+        <div className="go-back-button">
+          <button onClick={() => window.location.href = '/'} title="Go back">
+            <ArrowLeftCircle size={55} color="white" />
+          </button>
+        </div>
+        )}
+        <div className="iframe-container text-center">
+          <iframe frameborder="0" src="https://itch.io/embed-upload/12738277?color=121212" width="1280" height="740" allowfullscreen>
+            <a href="https://scranstudios.itch.io/fartboy-flies-again">Play FartBoy Flies Again! on itch.io</a>
+          </iframe>
+        </div>
       </Container>
     </section>
   )
